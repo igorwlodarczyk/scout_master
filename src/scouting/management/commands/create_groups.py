6687007b2e49ff396ba6b979d7eafb9475b018d1
Igor Wlodarczyk <wlodar.igor@gmail.com>
2023-11-19 21:42:08 +0100

    added command for creating user groups

---
+++ src/scouting/management/commands/create_groups.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group
+
+
+class Command(BaseCommand):
+    help = 'Creates groups to identify users'
+
+    def handle(self, *args, **options):
+        groups = ['Scout', 'Sports_director']
+
+        for group_name in groups:
+            Group.objects.get_or_create(name=group_name)
