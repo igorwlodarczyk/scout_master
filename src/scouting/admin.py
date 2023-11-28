2f8736743e65f221f31f2502254fe1b6283e33fa
Igor Wlodarczyk <wlodar.igor@gmail.com>
2023-11-28 21:14:12 +0100

    adjusted admin py file

--- src/scouting/admin.py
+++ src/scouting/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from .models import Country, League, Club, Player, ScoutReport
+from .models import Country, League, Club, Player, ScoutReport, Match
 
 # Register your models here.
 admin.site.register(Country)
@@ -8,3 +7,4 @@ admin.site.register(League)
 admin.site.register(Club)
 admin.site.register(Player)
 admin.site.register(ScoutReport)
+admin.site.register(Match)
