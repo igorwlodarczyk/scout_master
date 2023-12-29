67abd26be8ae4766c897314ce1a9e44cd67a1f01
Igor Wlodarczyk <wlodar.igor@gmail.com>
2023-12-29 17:57:25 +0100

    added slug for player model

--- src/scouting/models.py
+++ src/scouting/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -40,6 +41,10 @@ class Player(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.club.name}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 
 class Match(models.Model):
