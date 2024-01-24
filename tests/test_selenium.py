4b02675bfa04e2d201c34437a2e68a64e895a036
Igor Wlodarczyk <wlodar.igor@gmail.com>
2024-01-24 21:58:42 +0100

    refactor

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -4,13 +4,25 @@ from .const import *
 from time import sleep
 
 
-@pytest.mark.parametrize("username, password", users)
-def test_login(driver, username, password):
-    driver.get(website_base_url)
+def login_procedure(driver, username, password):
     element_username = driver.find_element(By.NAME, "username")
     element_username.send_keys(username)
     element_password = driver.find_element(By.NAME, "password")
     element_password.send_keys(password)
     submit_button = driver.find_element(By.CSS_SELECTOR, ".btn.btn-success")
     submit_button.click()
+
+
+@pytest.mark.dependency()
+@pytest.mark.parametrize("username, password", users)
+def test_login(driver, username, password):
+    driver.get(website_base_url)
+    login_procedure(driver, username, password)
     assert driver.current_url == website_base_url
+
+
+@pytest.mark.dependency(
+    depends=[f"test_login[{user}-{password}]" for user, password in users]
+)
+def test_placeholder():
+    ...
