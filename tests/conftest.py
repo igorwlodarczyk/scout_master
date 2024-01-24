7200b76d229702a62ea30e85e3098658df0132b7
Igor Wlodarczyk <wlodar.igor@gmail.com>
2024-01-24 21:35:11 +0100

    added first test for checking login page

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,11 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def driver(request):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_driver_path = ChromeDriverManager().install()
+    chrome_service = webdriver.chrome.service.Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=chrome_service)
     yield driver
     driver.close()
 
--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,5 +1,17 @@
 import pytest
+from selenium.webdriver.common.by import By
+from .const import *
+from time import sleep
 
 
-def test_open_page(driver):
-    driver.get("http://127.0.0.1:8000")
+@pytest.mark.parametrize("username, password", users)
+def test_login(driver, username, password):
+    driver.get(webiste_base_url)
+    element_username = driver.find_element(By.NAME, "username")
+    element_username.send_keys(username)
+    element_password = driver.find_element(By.NAME, "password")
+    element_password.send_keys(password)
+    submit_button = driver.find_element(By.CSS_SELECTOR, ".btn.btn-success")
+    submit_button.click()
+
+    sleep(4)
