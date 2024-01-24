15afe69c8fe4a8997dedc0fe2b0893c5495da185
Igor Wlodarczyk <wlodar.igor@gmail.com>
2024-01-24 22:08:49 +0100

    added fixture for running django server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
+import subprocess
+import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 
 
 @pytest.fixture(scope="function")
@@ -10,3 +13,13 @@ def driver(request):
     driver = webdriver.Chrome(service=chrome_service)
     yield driver
     driver.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def run_server():
+    manage_py_file_path = os.path.join("src", "manage.py")
+    server_process = subprocess.Popen(["python3", manage_py_file_path, "runserver"])
+    sleep(2)
+    yield server_process
+    server_process.terminate()
+    server_process.wait(timeout=2)
